c00632ac8f81319e24dcb5206240883aeaab41fc
Joiyad <20bee012@nith.ac.in>
2022-11-19 12:16:45 +0530

    background updated

--- pages/connect.tsx
+++ pages/connect.tsx
@@ -1,7 +1,10 @@
 import Navbar from '../components/sidebar';
 import Contact from '../components/contact.jsx';
+import { useState } from 'react';
+import { useRouter } from 'next/router';
 
 const follow = () => {
+  const router = useRouter()
   return (
     <div>
         <Navbar />
