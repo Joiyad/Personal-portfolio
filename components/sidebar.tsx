ff2afd5bf6b7e69276844fc8dad8bd0a41518c91
Joiyad <20bee012@nith.ac.in>
2023-02-20 22:40:54 +0530

    extra files deleted

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -17,7 +17,7 @@ const Navbar = () => {
       {/* full navbar */}
       <div className="hidden md:block z-20 fixed w-16 hover:w-44 h-screen bg-transparent hover:bg-slate-900 bg-opacity-60 hover:transition-all overflow-hidden hover:border-r-4 border-white border-opacity-10" id="menuN" >
         {navData.map(({id, link, icon, title}) => (
-        <nav>
+        <nav key={id}>
           <div className="flex flex-col text-gray-200">
             <Link key={id} href={link} ><a className="p-4 rounded-md bg-gradient-to-r hover:from-blue-400 hover:to-purple-500 flex flex-row items-center">{icon}<p>{title}</p></a></Link>
           </div>
@@ -28,7 +28,7 @@ const Navbar = () => {
       {/* menu mobile */}
       <div className={isOpen ? "hidden" : "md:block z-20 fixed w-full h-full bg-slate-900 bg-opacity-90 hover:transition-all overflow-hidden hover:border-r-2 border-white border-opacity-10"} id="menuN" >
       {navData.map(({id, link, icon, title}) => (
-        <nav>
+        <nav key={id}>
           <div className="flex flex-col text-gray-200">
             <Link key={id} href={link} ><a className="p-4 rounded-md bg-gradient-to-r hover:from-blue-400 hover:to-purple-500 flex flex-row items-center">{icon}<p>{title}</p></a></Link>
           </div>
